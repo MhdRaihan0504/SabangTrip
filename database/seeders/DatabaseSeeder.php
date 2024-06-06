ab1be4a62a3f02306a10d865a111b0efa9639863
meutiaainii <meutiaaini@mhs.usk.ac.id>
2024-06-06 14:53:28 +0700

    feat: add seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,43 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'nama' => 'user',
             'email' => 'tes@gmail.com',
+            'role' => 'user',
             'password' => Hash::make('123'),
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',
+            'password' => Hash::make('123'),
+        ]);
+
+        DB::table('testimonial')->insert([
+            'name' => 'Meutia Aini',
+            'rating' => 5,
+            'message' => 'bagus bangett, pesan tiket ke sabang jadi lebih mudah dan cepat',
+            'created_at' => '2021-08-01 00:00:00',
+        ]);
+
+        DB::table('testimonial')->insert([
+            'name' => 'Rizki Julina',
+            'rating' => 5,
+            'message' => 'Pengalaman dengan Sabang Trip sangat bagus. Situs webnya informatif dan mudah digunakan. Pilihan paket wisatanya menarik',
+            'created_at' => '2021-08-02 00:00:00',
+        ]);
+
+        DB::table('testimonial')->insert([
+            'name' => 'Iffatun',
+            'rating' => 3,
+            'message' => 'bagus, memudahkan dalam memesan tiket',
+            'created_at' => '2021-08-03 00:00:00',
+        ]);
+
+        DB::table('testimonial')->insert([
+            'name' => 'Nazwa Salsabila',
+            'rating' => 5,
+            'message' => 'Situs web ini memiliki banyak opsi paket wisata yang menarik dan harga yang terjangkau. Saya tidak sabar untuk melakukan perjalanan lagi dengan Sabang Trip!!!',
+            'created_at' => '2021-08-04 00:00:00',
         ]);
     }
 }
