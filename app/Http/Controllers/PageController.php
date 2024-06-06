31a44240041fea15c4358e1071dfd534e725c182
Iffatunnisaa <iffatun@mhs.usk.ac.id>
2024-06-06 14:24:29 +0700

    feat: add some models

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Testimonial;
+use App\Models\Discover;
 
 class PageController extends Controller
 {
@@ -13,7 +15,9 @@ class PageController extends Controller
 
     public function welcome()
     {
-        return view('index', ['title' => 'Dashboard']);
+        $discovers = Discover::all();
+        $testimonials = Testimonial::all();
+        return view('index', ['title' => 'Dashboard', 'testimonials' => $testimonials, 'discovers' => $discovers]);
     }
 
     public function about()
