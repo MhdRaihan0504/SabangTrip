90c371a4139da8eb08ac9df5424238f5869cb98f
meutiaainii <meutiaaini@mhs.usk.ac.id>
2024-06-06 14:55:00 +0700

    feat: fix navbar route

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,27 +2,27 @@
 @if (Auth::user()->role == 'user')
 <nav class="bg-[#EEE9DA] p-4 shadow-xl">
     <div class="container mx-auto flex justify-between items-center">
-        <a class="text-[#265073] font-bold text-xl" href="/index">
+        <a class="text-[#265073] font-bold text-xl" href="{{ route('index') }}">
             <img src="{{ asset('image/logo.svg') }}" class="w-1/2 h-1/2" alt="Logo">
         </a>
         <ul class="flex space-x-16">
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/index">Home</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('index') }}">Home</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/discover">Discover</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('discover') }}">Discover</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/etiket">E-Ticket</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('etiket') }}">E-Ticket</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="#">Testimonial</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('testimonial') }} ">Testimonial</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/about">About</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('about') }}">About</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="#">
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('logout') }}">
                     <img src="{{ asset('image/pp.svg') }}" class="w-8 h-8 rounded" alt="Profile Picture">
                 </a>
             </li>
@@ -33,27 +33,27 @@
 @elseif (Auth::user()->role == 'admin')
 <nav class="bg-[#EEE9DA] p-4 shadow-xl">
     <div class="container mx-auto flex justify-between items-center">
-        <a class="text-[#265073] font-bold text-xl" href="/index">
+        <a class="text-[#265073] font-bold text-xl" href="{{ route('index') }}">
             <img src="{{ asset('image/logo.svg') }}" class="w-1/2 h-1/2" alt="Logo">
         </a>
         <ul class="flex space-x-16">
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/index">Home</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('index') }}">Home</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/discover">Discover</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('discover') }}">Discover</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/tiket">E-Ticket</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('tiket.request') }}">E-Ticket</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="#">Testimonial</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('testimonial') }}">Testimonial</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/about">About</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('about') }}">About</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/logout">
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="{{ route('logout') }}">
                     <img src="{{ asset('image/pp.svg') }}" class="w-8 h-8 rounded" alt="Profile Picture">
                 </a>
             </li>
@@ -70,19 +70,10 @@
         </a>
         <ul class="flex space-x-16">
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/">Home</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/register">Sign Up</a>
             </li>
             <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="#">Discover</a>
-            </li>
-            <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="#">E-Ticket</a>
-            </li>
-            <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="#">Testimonial</a>
-            </li>
-            <li>
-                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="#">About</a>
+                <a class="text-[#265073] hover:text-blue-300 font-bold text-xl" href="/login">Login</a>
             </li>
         </ul>
     </div>
